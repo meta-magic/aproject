3e57c27ff56f6e13fb5a61e91f31487b63c1b2be
deepali-arvind <deepali-arvind>
2018-07-11 07:16:20 +0000

    Initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,5 @@
 /**
- * Created by Sagar Jadhav.
+ * Created by sagar on 12/2/18.
  */
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
@@ -12,16 +12,8 @@ import {AmexioWidgetModule} from "amexio-ng-extensions";
 const routes: Routes = [
   { path: '', component:HomeComponent,
     children: [
-          	{
-     	path: 'core/aprj', loadChildren: './../pages/testyy/core/aprj/aprj.module#AprjModule'
-     	},	
-	     	{
-     	path: 'core/asfafui', loadChildren: './../pages/testyy/core/asfafui/asfafui.module#AsfafuiModule'
-     	},	
-	     	{
-     	path: 'dsf/uuu', loadChildren: './../pages/adsdda/dsf/uuu/uuu.module#UuuModule'
-     	},	
-	    ]
+
+    ]
   }
 ];
 
