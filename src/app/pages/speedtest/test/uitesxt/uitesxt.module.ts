b270152b0a41322d9541b43c1a5cd148389f2ba8
deepali-arvind <deepali-arvind>
2018-07-18 11:20:14 +0000

    Initial commit

---
+++ src/app/pages/speedtest/test/uitesxt/uitesxt.module.ts
@@ -0,0 +1,38 @@
+/**
+ * Created by: deepali arvind
+ * Date: 18/07/2018
+ * Organization: Individual	
+ */
+import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import { UitesxtComponent } from './uitesxt.component';
+import {AmexioWidgetModule, AmexioChartsModule,AmexioDashBoardModule,AmexioMapModule,CommonDataService} from "amexio-ng-extensions";
+import {RouterModule, Routes} from "@angular/router";
+import {HttpClientModule } from '@angular/common/http';
+const routes: Routes = [
+{ 
+	path: '', component: UitesxtComponent 
+}
+];
+@NgModule(
+{
+	declarations:[
+	    UitesxtComponent
+	 ],
+	imports: [
+	FormsModule,
+	CommonModule, 
+	HttpClientModule,
+	RouterModule.forChild(routes),
+	AmexioWidgetModule,
+	AmexioChartsModule,
+	AmexioDashBoardModule,
+	AmexioMapModule	
+	],
+	providers: [CommonDataService]
+}
+)
+export class UitesxtModule 
+{ 
+}
